46eb7353f55ad23ed19212972a819e9408695e6d
Lvzbel <rcoto@protonmail.com>
2020-05-23 20:37:39 -0700

    Feat: bootstrap implemented

--- functions.php
+++ functions.php
@@ -2,6 +2,10 @@
 
 // Enqueue Styles and Scripts
 function add_theme_scripts() {
+  // BooStrap
+  wp_enqueue_style( 'bootstrap_css', get_template_directory_uri() . '/node_modules/bootstrap/dist/css/bootstrap.min.css', array(), '4.5.0', 'all');
+
+  wp_enqueue_script( 'bootstrap_script', get_template_directory_uri() . '/node_modules/bootstrap/dist/js/bootstrap.min.js', array ('jquery'), '1.0.0', true);
   // Vendor CSS
   wp_enqueue_style( 'vendor_styles', get_template_directory_uri() . '/dist/vendor.css', array(), '1.0.0', 'all');
   // Custom Styles CSS
