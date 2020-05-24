3d627cfb37bc0d9b916202739a3735194c06e13c
Lvzbel <rcoto@protonmail.com>
2020-05-23 23:29:28 -0700

    Styles: hero done

--- front-page.php
+++ front-page.php
@@ -5,7 +5,7 @@
 <div class="hero" style="background-image: url(<?php echo get_field('hero_image')['url'] ?>)">
   <div class="hero__overlay"></div>
   <div class="hero__content">
-    <h1 class="hero__header display-2 text-light"><?php the_field('hero_header') ?></h1>
+    <h1 class="hero__header text-light"><?php the_field('hero_header') ?></h1>
     <p class="hero__lead lead text-light mb-4"><?php the_field('hero_lead') ?></p>
     <button class="btn btn-primary btn-lg">Order Now</button>
   </div>
